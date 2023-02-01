8312fef2536bcfe041a677a86dd7b15f3ad7ad9c
aleino0 <113008053+aleino0@users.noreply.github.com>
2023-02-01 09:17:47 +0100

    Add files via upload

---
+++ KlasaDirectory/namespace_systemIO/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace namespace_systemIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("unesite putenju datoteke");
+            string putanja = Console.ReadLine();
+
+            if(Directory.Exists(putanja))
+            {
+                foreach (string datoteke in Directory.GetFiles(putanja))
+                {
+                    Console.WriteLine(datoteke);
+                }
+            }
+
+            string putanjaPoddirektorija=putanja.Insert(putanja.Length,"//TEST");
+            if (!Directory.Exists(putanjaPoddirektorija))
+            {
+                Directory.CreateDirectory(putanjaPoddirektorija);
+            }
+            else
+            {
+                Console.WriteLine("Direktorij već postoji!");
+            }
+            Console.WriteLine("svi direktoriji");
+            foreach(string direktorij in Directory.GetDirectories(putanja))
+            {
+                Console.WriteLine(direktorij);
+            }
+
+            Directory.Delete(putanjaPoddirektorija);
+            Console.WriteLine("'TEST' je obrisan");
+            foreach (string direktorij in Directory.GetDirectories(putanja))
+            {
+                Console.WriteLine(direktorij);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
