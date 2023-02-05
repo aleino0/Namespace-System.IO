8f5c4ab40d6887eb84903511e785e7e323568918
aleino0 <113008053+aleino0@users.noreply.github.com>
2023-02-05 19:33:09 +0100

    Add files via upload

---
+++ SystemIO_klasaFile/SystemIO_klasaFile/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SystemIO
+{
+    internal class klasaFile
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upiši svoje ime: ");
+            string ime = Console.ReadLine();
+
+            string datoteka = "ime.txt";
+
+            if (File.Exists(datoteka))
+            {
+                if (!Directory.Exists("backup"))
+                {
+                    Directory.CreateDirectory("backup");
+                }
+
+                File.Copy(datoteka, @"backup\ime_"
+                    + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")
+                    + ".txt");
+
+                File.Delete(datoteka);
+            }
+
+            File.WriteAllText(datoteka, ime);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ SystemIO_streamPisac/SystemIO_streamPisac/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemIO
+{
+    internal class streamPisac
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upišite ime: ");
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Upišite prezime: ");
+            string prezime = Console.ReadLine();
+
+            StreamWriter sw = new StreamWriter(
+                @"c:\prviDir\mojaDatoteka.txt");
+
+            sw.WriteLine("Ime: {0}", ime);
+            sw.WriteLine("Prezime: {0}", prezime);
+            sw.Close();
+
+
+            StreamReader sr = new StreamReader(
+                @"c:\prviDir\mojaDatoteka.txt");
+
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+
+            sr.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
